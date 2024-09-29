20129bd30ef64b547b185f21284079b92b906959
Cinta Ramayanti <cinta.ramayanti@gmail.com>
2024-09-29 16:08:44 +0700

    profile fix

--- app/src/main/java/com/example/flower/ProfileActivity.kt
+++ app/src/main/java/com/example/flower/ProfileActivity.kt
@@ -1,20 +1,37 @@
 package com.example.flower
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.widget.Toolbar
 
 class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
 
+        val toolbar: Toolbar = findViewById(R.id.toolbar_profile)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.baseline_arrow_back_24)
+
         val tvUserName: TextView = findViewById(R.id.tv_user_name)
         val imgProfile: ImageView = findViewById(R.id.img_user_profile)
 
-        // Ganti sesuai dengan nama pengguna dan gambar profil yang diinginkan
         tvUserName.text = "Cinta Ramayanti"
         imgProfile.setImageResource(R.drawable.profile_picture)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                onBackPressed()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
